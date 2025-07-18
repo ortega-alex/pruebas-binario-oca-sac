f0df19d8abdc9b092757dc926aad3724bf8ef762
Alex Ortega <mortegalex27@outlook.es>
2025-07-18 13:37:10 -0600

    uri

--- api-rest/dist/config/envs.js
+++ api-rest/dist/config/envs.js
@@ -5,15 +5,15 @@ require("dotenv/config");
 const joi = require("joi");
 const envsSchema = joi
     .object({
-    MONGO_URI: joi.string().required()
+    MONGO_URI: joi.string()
 })
     .unknown();
 const { error, value } = envsSchema.validate(process.env);
 if (error) {
     throw new Error(`Config validation error: ${error.message}`);
 }
-const envVars = value;
+const envVars = value || 'mongodb://192.168.5.3:27017/centralizador';
 exports.envs = {
-    MONGO_URI: envVars.MONGO_URI || 'mongodb://192.168.5.3:27017/centralizador'
+    MONGO_URI: envVars.MONGO_URI
 };
 //# sourceMappingURL=envs.js.map
