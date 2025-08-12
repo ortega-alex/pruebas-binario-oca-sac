85419180af7344e028bfa9fc570ca06c8f51bcf2
Alex Ortega <mortegalex27@outlook.es>
2025-08-12 17:33:23 -0600

    extension de tiempo de pruebas

---
+++ assets/Loading-CTUouFAF.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DB_ClH5N.js";const o=()=>s.jsx("span",{className:"loader"});export{o as L};
--- assets/Loading-DQHppkBo.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-BiUOhA_2.js";const o=()=>s.jsx("span",{className:"loader"});export{o as L};
